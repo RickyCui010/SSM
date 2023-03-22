3b9a445d2a423b84772983db83af97f1c85467fe
ricky <Roll08@outlook.com>
2023-03-22 20:09:44 +0800

    Initial commit

---
+++ mybatis_parameter/src/test/java/com/ricky/mybatis/test/SelectMapperTest.java
@@ -0,0 +1,64 @@
+package com.ricky.mybatis.test;
+
+import com.ricky.mybatis.mapper.SelectMapper;
+import com.ricky.mybatis.pojo.User;
+import com.ricky.mybatis.utils.SqlSessionUtil;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.session.SqlSession;
+import org.junit.Test;
+
+import java.util.List;
+import java.util.Map;
+
+public class SelectMapperTest {
+
+    @Test
+    public void testGetUserById(){
+        SqlSession sqlSession = SqlSessionUtil.getSqlSession();
+        SelectMapper mapper = sqlSession.getMapper(SelectMapper.class);
+        User user = mapper.getUserById(1);
+        System.out.println(user);
+    }
+
+    @Test
+    public void testGetAllUser(){
+        SqlSession sqlSession = SqlSessionUtil.getSqlSession();
+        SelectMapper mapper = sqlSession.getMapper(SelectMapper.class);
+        List<User> list = mapper.getAllUser();
+        list.forEach(System.out::println);
+    }
+
+    @Test
+    public void testGetCount(){
+        SqlSession sqlSession = SqlSessionUtil.getSqlSession();
+        SelectMapper mapper = sqlSession.getMapper(SelectMapper.class);
+        Integer count = mapper.getCount();
+        System.out.println(count);
+    }
+
+    @Test
+    public void testGetUserByIdToMap(){
+        SqlSession sqlSession = SqlSessionUtil.getSqlSession();
+        SelectMapper mapper = sqlSession.getMapper(SelectMapper.class);
+        Map<String, Object> userByIdToMap = mapper.getUserByIdToMap(1);
+        System.out.println(userByIdToMap);
+    }
+
+    @Test
+    public void testGetAllUseToMap(){
+        SqlSession sqlSession = SqlSessionUtil.getSqlSession();
+        SelectMapper mapper = sqlSession.getMapper(SelectMapper.class);
+        List<Map<String, Object>> list = mapper.getAllUseToMap();
+        list.forEach(System.out::println);
+    }
+
+
+    @Test
+    public void testGetAllUserToMapValue(){
+        SqlSession sqlSession = SqlSessionUtil.getSqlSession();
+        SelectMapper mapper = sqlSession.getMapper(SelectMapper.class);
+        Map<String, Object> map = mapper.getAllUserToMapValue();
+        System.out.println(map);
+    }
+
+}
